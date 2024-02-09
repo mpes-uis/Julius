ce5753e128605d2aa9f5e9182fb92528f4badb45
HeitorQuartezani <151958109+HeitorQuartezani@users.noreply.github.com>
2024-02-09 11:11:46 -0300

    Delete scr_R directory

--- scr_R/dados.R
+++
@@ -1,5 +0,0 @@
-prefeituras <- read.csv("dados/prefeituras.csv")
-
-prefeituras_portaltp <- subset(prefeituras, empresa == "portaltp")
-
-metodos <- read.csv("dados/APImethods.csv")
--- scr_R/pacotes.R
+++
@@ -1,8 +0,0 @@
-
-install.packages("httr")
-install.packages("jsonlite")
-install.packages("RSQLite")
-
-library(httr)
-library(jsonlite)
-library(RSQLite)
